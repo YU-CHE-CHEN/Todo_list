b806934cf10be3fcf0ff9faa57f984261006911e
YU-CHE CHEN <job047587213@hotmail.com>
2020-08-07 02:22:57 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 //connect to mongodb
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -15,7 +18,7 @@ db.once('open', () => {
 
 //setting route
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.render('index')
 })
 
 //setting port
