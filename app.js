d13ac84f35e5cfd0f33446c1a3556b1025a9c4db
YU-CHE CHEN <job047587213@hotmail.com>
2020-08-08 00:02:45 +0800

    feat: add the detail

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 //setting port
 app.listen(3000, () => {
   console.log('App is running on localhost: 3000.')
