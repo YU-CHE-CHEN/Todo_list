5af435ae0454c73efa45346dd59248771b6b2958
YU-CHE CHEN <job047587213@hotmail.com>
2020-08-12 00:18:58 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
+const app = express()
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -26,56 +16,6 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-//add the 'new' route
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  return Todo.create({ name: name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-//setting port
 app.listen(3000, () => {
   console.log('App is running on localhost: 3000.')
 })
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,14 +1,7 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-//connect to mongodb
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: 'name-' + i })
   }
